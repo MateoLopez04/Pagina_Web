ba1eae586af1adb2309e92c4a92a04149d4f291f
MateoLopez04 <nestormdiazl@autonoma.edu.co>
2019-06-01 11:12:47 -0500

    interfaz

---
+++ Web-Tienda/carrusel.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+//   // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.container .carrusel').carousel();
+//   });
+
+var instance = M.Carousel.init({
+    fullWidth: true
+  });
